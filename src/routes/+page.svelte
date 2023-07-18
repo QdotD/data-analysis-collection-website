376c37264a423b3dd75c5d10ccbd6cd7b6cd183b
QdotD <quinton.digital@protonmail.com>
2023-07-18 15:16:00 -0700

    refactored code to work with supabase cause google SQL is way too expensive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,333 +1,180 @@
 <script>
-  import { onMount } from "svelte";
+  // imports
   import { isEmail, isMobilePhone } from "validator";
   import { PhoneNumberFormat, PhoneNumberUtil } from "google-libphonenumber";
+  import { createClient } from "@supabase/supabase-js";
 
-  const phoneUtil = PhoneNumberUtil.getInstance();
-
-  let firstName = "";
-  let lastName = "";
+  // form vars
+  let first_name = "";
+  let last_name = "";
   let email = "";
-  let phoneNumber = "";
+  let phone_number = "";
+  let age = "";
   let job = "";
+  let salary_usd = "";
   let country = "";
+
+  // status vars
   let statusMessage = "";
   let formValid = false;
   let isSuccess = false;
-  let SalaryUSD = "";
   let isSubmitting = false;
   let submitButtonText = "Submit";
-  let age = "";
 
-  let errors = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNumber: "",
-    job: "",
-    country: "",
-    SalaryUSD: "",
-    age: "",
-  };
+  // supabase setup
+  let supabase;
 
-  const checkValidity = () => {
-    errors.firstName = firstName ? "" : "First name is required.";
-    errors.lastName = lastName ? "" : "Last name is required.";
-    errors.email = isEmail(email) ? "" : "Email is not valid.";
-    errors.age = age ? "" : "Age is required.";
-
-    let phoneResult = cleanAndValidatePhone(phoneNumber);
-    errors.phoneNumber = phoneResult.error || "";
-    if (!phoneResult.error) {
-      phoneNumber = phoneResult.cleanedPhone;
-    }
-
-    errors.job = job ? "" : "Job is required.";
-    errors.country = country ? "" : "Country is required.";
-
-    errors.SalaryUSD =
-      SalaryUSD === undefined ||
-      SalaryUSD === null ||
-      (!isNaN(SalaryUSD) && Number(SalaryUSD) >= 0)
-        ? ""
-        : "SalaryUSD is not valid.";
-
-    formValid = Object.values(errors).every((error) => error === "");
-  };
-
-  function cleanAndValidatePhone(phoneNumber, defaultCountry = "US") {
-    let parsedNumber;
-    let cleanedPhone;
-
-    try {
-      // Remove extension if it exists
-      phoneNumber = phoneNumber.split("x")[0];
-      parsedNumber = phoneUtil.parseAndKeepRawInput(
-        phoneNumber,
-        defaultCountry
-      );
-    } catch (err) {
-      console.error("Invalid phone number format: ", err);
-      return { error: "Invalid phone number format." };
-    }
-
-    // Convert to E.164 format without checking if it's valid
-    cleanedPhone = phoneUtil.format(parsedNumber, PhoneNumberFormat.E164);
-    return { cleanedPhone };
-  }
-
-  onMount(() => {
-    checkValidity();
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL;
+  const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
+  supabase = createClient(supabaseUrl, supabaseKey, {
+    persistSession: false,
   });
 
-  async function handleSubmit() {
-    if (!formValid || isSubmitting) {
-      statusMessage = "Form is not valid. Please check your entries.";
-      checkValidity();
-      isSuccess = false;
-      return;
-    }
+  // phone number validation
+  const phoneUtil = PhoneNumberUtil.getInstance();
 
-    isSubmitting = true; // set isSubmitting to true when a submission starts
-    submitButtonText = "Submitting..."; // change the text displayed on the button when a submission starts
+  // Function to verify if email exists
+  async function emailExists() {
+    let { data: users, error } = await supabase
+      .from("users")
+      .select("email")
+      .eq("email", email);
 
-    // Clear all previous errors
-    errors = {
-      firstName: "",
-      lastName: "",
-      email: "",
-      phoneNumber: "",
-      job: "",
-      country: "",
-      SalaryUSD: "",
-      age: "",
-    };
+    return error || users.length > 0 ? true : false;
+  }
 
-    checkValidity();
-    if (!formValid) {
-      statusMessage = "Form is not valid. Please check your entries.";
-      isSuccess = false;
-      return;
-    }
+  // Function to submit the form data to Supabase
+  async function submitToSupabase() {
+    const { error: insertError } = await supabase.from("users").insert({
+      first_name,
+      last_name,
+      email,
+      phone_number,
+      job,
+      country,
+      salary_usd,
+      age,
+    });
 
-    try {
-      const emailResponse = await fetch(
-        `/api/validateEmail?email=${encodeURIComponent(email)}`
-      );
-
-      if (!emailResponse.ok) {
-        throw new Error(
-          `Server responded with status code ${emailResponse.status}`
-        );
-      }
-
-      const emailData = await emailResponse.json();
-
-      if (emailData.exists) {
-        errors.email = "Email already exists.";
-        isSuccess = false;
-        return;
-      }
-
-      const response = await fetch("/api/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          phoneNumber,
-          job,
-          country,
-          SalaryUSD,
-          age,
-        }),
-      });
-
-      if (response.ok) {
-        statusMessage = "User added successfully";
-        isSuccess = true; // Set isSuccess to true when form submission is successful
-
-        // Clear all fields after successful submission
-        firstName = "";
-        lastName = "";
-        email = "";
-        phoneNumber = "";
-        job = "";
-        country = "";
-        SalaryUSD = "";
-        age = "";
-
-        // Reset touchedFields object
-        touchedFields = {
-          firstName: false,
-          lastName: false,
-          email: false,
-          phoneNumber: false,
-          job: false,
-          country: false,
-          SalaryUSD: false,
-          age: false,
-        };
-      } else {
-        statusMessage = "An error occurred";
-        isSuccess = false; // Set isSuccess to false when an error occurred
-      }
-    } catch (error) {
-      console.error("Error occurred:", error);
-      statusMessage = "An error occurred. Please try again later.";
-      isSuccess = false;
-    } finally {
-      isSubmitting = false; // set isSubmitting to false when a submission ends, whether it succeeded or failed
-      submitButtonText = "Submit"; // change the text displayed on the button back to "Submit" when a submission ends
+    if (insertError) {
+      console.error("Error inserting into users:", insertError);
+      return true;
+    } else {
+      return false;
     }
   }
 
-  let touchedFields = {
-    firstName: false,
-    lastName: false,
-    email: false,
-    phoneNumber: false,
-    job: false,
-    country: false,
-    SalaryUSD: false,
-    age: false,
+  // reset form
+  const resetForm = () => {
+    first_name = "";
+    last_name = "";
+    email = "";
+    phone_number = "";
+    age = "";
+    job = "";
+    salary_usd = "";
+    country = "";
   };
 
-  const handleBlur = (fieldName) => {
-    touchedFields[fieldName] = true;
-    if (fieldName === "phoneNumber") {
-      let phoneResult = cleanAndValidatePhone(phoneNumber);
-      if (!phoneResult.error) {
-        phoneNumber = phoneResult.cleanedPhone;
-      }
+  // Main function to handle the form submission
+  async function handleSubmit() {
+    isSubmitting = true;
+    submitButtonText = "Submitting...";
+
+    // Verify if email already exists
+    if (await emailExists()) {
+      statusMessage = "Email already exists.";
+      isSuccess = false;
+      isSubmitting = false;
+      submitButtonText = "Submit";
+      return;
     }
-    checkValidity();
-  };
 
-  // Function to remove unwanted characters
-  const sanitizeInput = (value) => {
-    return value.replace(/\./g, "");
-  };
-
-  const handleInput = (fieldName, event) => {
-    if (fieldName === "phoneNumber") {
-      phoneNumber = event.target.value;
+    // Post the form data to Supabase
+    if (await submitToSupabase()) {
+      statusMessage = "An error occurred";
+      isSuccess = false;
     } else {
-      // Handle other fields if needed
+      statusMessage = "User added successfully";
+      isSuccess = true;
+      isSubmitting = false;
+      submitButtonText = "Submit";
+      resetForm();
     }
-    checkValidity(); // validate the form fields each time the input changes
-  };
+  }
 </script>
 
 <form id="form_id" on:submit|preventDefault={handleSubmit}>
   <input
     id="first_name_input_id"
     type="text"
-    bind:value={firstName}
-    on:blur={() => handleBlur("firstName")}
+    bind:value={first_name}
     placeholder="First Name"
     required
   />
-  {#if errors.firstName && touchedFields.firstName}<p class="error">
-      {errors.firstName}
-    </p>{/if}
 
   <input
     id="last_name_input_id"
     type="text"
-    bind:value={lastName}
-    on:blur={() => handleBlur("lastName")}
+    bind:value={last_name}
     placeholder="Last Name"
     required
   />
-  {#if errors.lastName && touchedFields.lastName}<p class="error">
-      {errors.lastName}
-    </p>{/if}
 
   <input
     id="age_input_id"
     type="number"
     bind:value={age}
-    on:blur={() => handleBlur("age")}
     placeholder="Age"
     required
   />
-  {#if errors.age && touchedFields.age}<p class="error">
-      {errors.age}
-    </p>{/if}
 
   <input
     id="email_input_id"
     type="email"
     bind:value={email}
-    on:blur={() => handleBlur("email")}
     placeholder="Email"
     required
   />
-  {#if errors.email && touchedFields.email}<p class="error">
-      {errors.email}
-    </p>{/if}
 
   <input
     id="phone_number_input_id"
     type="tel"
-    bind:value={phoneNumber}
-    on:blur={() => handleBlur("phoneNumber")}
-    on:input={(event) => handleInput("phoneNumber", event)}
+    bind:value={phone_number}
     placeholder="Phone Number"
     required
   />
-
-  {#if errors.phoneNumber && touchedFields.phoneNumber}<p class="error">
-      {errors.phoneNumber}
-    </p>{/if}
 
   <input
     id="job_input_id"
     type="text"
     bind:value={job}
-    on:blur={() => handleBlur("job")}
     placeholder="Job"
     required
   />
-  {#if errors.job && touchedFields.job}<p class="error">{errors.job}</p>{/if}
 
   <input
     id="country_input_id"
     type="text"
     bind:value={country}
-    on:blur={() => handleBlur("country")}
     placeholder="Country"
     required
   />
-  {#if errors.country && touchedFields.country}<p class="error">
-      {errors.country}
-    </p>{/if}
 
   <input
-    id="SalaryUSD_input_id"
+    id="salary_usd_input_id"
     type="number"
-    bind:value={SalaryUSD}
-    on:blur={() => handleBlur("SalaryUSD")}
-    placeholder="SalaryUSD"
+    bind:value={salary_usd}
+    placeholder="salary_usd"
     required
   />
-  {#if errors.SalaryUSD && touchedFields.SalaryUSD}<p class="error">
-      {errors.SalaryUSD}
-    </p>{/if}
 
   {#if statusMessage}
     <p class={isSuccess ? "success" : "error"}>{statusMessage}</p>
   {/if}
 
-  <button
-    id="submit_button_id"
-    type="submit"
-    disabled={!formValid || isSubmitting}
-    class:submitting={isSubmitting}>{submitButtonText}</button
+  <button id="submit_button_id" type="submit" class:submitting={isSubmitting}
+    >{submitButtonText}</button
   >
 </form>
 
@@ -350,11 +197,6 @@
     background-color: #fff;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
     border-radius: 8px;
-  }
-
-  label {
-    margin-bottom: 10px;
-    font-size: 0.9em;
   }
 
   input {
@@ -386,7 +228,7 @@
   }
 
   button.submitting {
-    background-color: #999;
+    background-color: #c1c767;
   }
 
   button:disabled {
